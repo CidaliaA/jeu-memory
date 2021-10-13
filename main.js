2473f0bcaf83e954fa286a9abf4fa51fed29ac45
CidaliaA <cidalia.andre01gmail.com>
2021-10-13 15:18:05 +0200

    init

---
+++ main.js
@@ -0,0 +1,24 @@
+const divResultat = document.querySelector("#resultat");
+
+// variable qui permet de savoir ou en nest dans le jeu(trouver pair ou non)
+
+var tablJeu = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+];
+// premiere fonction:affichage jeu; accces tableau sans passer par fonction()
+
+afficherTableau();
+
+function afficherTableau(){
+    var txt ="coucou";
+
+    for( var i=0; i< afficherTableau.length; i++){
+
+    }
+    divResultat.innerHTML =txt;
+}
+
+
